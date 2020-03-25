7337662a84fcf9a00d85e4919a36865561018866
Joseph E Auz <joseph.auz77@gmail.com>
2020-03-25 21:40:42 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -45,11 +45,11 @@ def reduce_to_total(source_array, starting_point)
   total = 0 
   while i < source_array.length do 
     total += source_array[i]
-    if starting_point
-      total += starting_point
-    end 
     i += 1 
   end 
+  if starting_point
+      total += starting_point
+    end 
   total
 end 
 
