721d764d6e099497f8cead1672f0a56eed6269af
Joseph E Auz <joseph.auz77@gmail.com>
2020-03-25 21:31:12 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -41,16 +41,7 @@ def map_to_square(source_array)
 end 
 
 def reduce_to_total(source_array, starting_point)
-  i = 0
-  total = 0 
-  while i < source_array.length do 
-    total += source_array[i]
-    if starting_point != nil
-     total += starting_point
-    end 
-    i += 1 
-  end 
-  total
+  
 end 
 
 def reduce_to_all_true(source_array)
